5256a92ea5348e215c45bb6b3b7cc3aa8d132ab2
majunjie58 <510457845@qq.com>
2026-01-07 17:53:14 +0800

    update

---
+++ sjeuur/config.js
@@ -0,0 +1,13 @@
+/**
+ * picture naming rules:
+ * pc : [name].[ext] ./assets/section1-bg.png
+ * pc webp : [name].webp ./assets/section1-bg.webp
+ * mobile : [name]_mobile.[ext] ./assets/section1-bg_mobile.png
+ * mobile webp : [name]_mobile.webp ./assets/section1-bg_mobile.webp
+ */
+window.textConfig = {
+  section_1: {
+    title: "测试<br/>标题",
+    image_1: "./assets/section1-bg.png",
+  },
+};
