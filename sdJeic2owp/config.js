ec0c3f64b22812d326fd4aaa47a0c7925795fbb6
majunjie58 <510457845@qq.com>
2026-02-03 14:00:50 +0800

    update

---
+++ sdJeic2owp/config.js
@@ -0,0 +1,206 @@
+/**
+ * picture naming rules:
+ * pc : [name].[ext]
+ * pc webp : [name].webp
+ * mobile : [name]_mobile.[ext]
+ * mobile webp : [name]_mobile.webp
+ */
+window.znConfig = {
+  // kv
+  section1: {
+    title: "TECNO SPARK 50",
+    desc: "More Power, Now Tougher",
+    b1: "7000mAh",
+    b1Desc: "Mega Battery",
+    b2: "FreeLink 2.0",
+    b3: "50MP",
+    b3Desc: "FlashSnap",
+    // picture
+    kvImage: "./assets/section1-kv-7000.jpg",
+  },
+  // nav
+  section2: {
+    navText1: "7000mAh Mega Battery<br/>18W Fast Charging ",
+    navText2: "50MP FlashSnap",
+    navText3: "FreeLink 2.0<br/>Signal Enhancement Sensor",
+    navText4: '120Hz 6.78" Smooth<br/>Display',
+    navText5: "Infrared Remote Control",
+    navText6: "IP64 Dust & Water<br/>Resistance",
+    navText7: "Drop Resistance <br/>Up to 1.8m",
+    navText8: "TECNO AI",
+    navText9: "4 Years<br/>Lasting Fluency",
+    // picture
+    navImage1: "./assets/section2-nav-1.png",
+    navImage2: "./assets/section2-nav-2.jpg",
+    navImage3: "./assets/section2-nav-3.jpg",
+    navImage4: "./assets/section2-nav-4.jpg",
+    navImage5: "./assets/section2-nav-5.jpg",
+    navImage6: "./assets/section2-nav-6.jpg",
+    navImage7: "./assets/section2-nav-7.jpg",
+    navImage8: "./assets/section2-nav-8.jpg",
+  },
+  // color
+  section3: {
+    title: "POP<br/>IN DESIGN",
+    desc: "Vibrant Colors, Varied Choices",
+    colorBlueText: "Halo Blue",
+    colorBlueDesc: "Halo Blue, halo glows around deep blue.",
+    colorGreyText: "Titanium Grey",
+    colorGreyDesc: "Titanium-like grey, low-key and gentle.",
+    colorBlackText: "Ink Black",
+    colorBlackDesc: "Classic black, as simple and pure as ink.",
+    colorPurpleText: "Aurora Purple",
+    colorPurpleDesc: "Aurora Purple, soft and dreamy.",
+    colorPinkText: "Bloom Pink",
+    colorPinkDesc: "Bloom pink, like a flower awakening in the spring.",
+    colorBlueImage: "./assets/section3-phone-blue.png",
+    colorGreyImage: "./assets/section3-phone-grey.png",
+    colorBlackImage: "./assets/section3-phone-black.png",
+    colorPurpleImage: "./assets/section3-phone-purple.png",
+    colorPinkImage: "./assets/section3-phone-pink.png",
+  },
+  //battery
+  section4: {
+    title: "POWER<br/>IN BUILD",
+    batteryTitleBlue: "BETTER",
+    batteryTitle: "BATTERY EXPERIENCE",
+    batteryText: "7000mAh Mega Battery,<br/>18W Fast Charging",
+    batteryNotice:
+      "*Disclaimer:<br/>Data such as battery capacity, battery usage and video playback duration are all<br/>from TECNO Laboratory. Actual data may vary.",
+    batteryT1: "Faster and Longer",
+    batteryD1:
+      "With a large 7000mAh battery, it can endure over 28h continuous <br/>video play. Even after 5 years of use, more than 80% of the <br/>capacity remains. The AI charging system learns your habits. <br/>Its ultra-high cycle life ensures durability.*",
+    displayTitleBlue: "SMOOTH",
+    displayTitle: "Display",
+    displayDesc1: "120Hz ",
+    displayDescPurple: '6.78"',
+    displayDesc2: "Smooth Display",
+    displayD1: "Smoother and Sharper Experience",
+    displayD2:
+      "The display delivers sharper details. <br/>Supports up to 120Hz adaptive refresh rate for smoother <br/>experience when playing games and watching videos.",
+    cpuTitleBlue: "HIGH",
+    cpuTitle: "PERFORMANCE",
+    cpuDesc: "MediaTek Helio <br />G81 Processor",
+    cpuT1: "Octa-core high-performance",
+    cpuD1:
+      "The eight-core high-performance processor runs smoothly. <br/>Its low-power design extends battery life and can easily <br/>handle daily tasks.",
+    freeLinkTitleBlue: "ENHANCED",
+    freeLinkTitle: "COMMUNICATION CAPABILITY",
+    freeLinkDesc: "FreeLink 2.0",
+    freeLinkT1: "Up to 1.5 Kilometers*<br />Free Connect without Signal",
+    freeLinkD1:
+      "In situations where there is no network coverage or the network is weak, <br/>direct communication between Bluetooth devices can achieve two-way <br/>voice calls, sending and receiving text & voice messages and photos. <br/>In addition, the phone will be equipped with a signal enhancement seneor <br/>developed by TECNO, which can significantly improve the communication <br/>experience within a range of up to 1.5 km in weak network environments.",
+    freeLinkNotice:
+      "*Disclaimer:<br/>A good experlence can be obtained within a 1.5 kilometers open and unobstructed area. The actual effect may vary depending on the usage environment.",
+    // picture
+    batteryImage: "./assets/section4-battery.png",
+    displayImage: "./assets/section4-display.jpg",
+    cpuImage: "./assets/section4-cpu.jpg",
+    cpuImageUp: "./assets/section4-cpu-up.png",
+    signalImage: "./assets/section4-signal.jpg",
+  },
+  // in use
+  section5: {
+    title: "Excellence <br/>in Use",
+    UsedescBlue: "Super ",
+    Usedesc: "Durability",
+    UseB1Title: "IP64 Dust & Water <br/>Resistance",
+    UseB1Desc: "Protected Against Dust and Splashes",
+    UseB1Text:
+      "Effectively blocks dust intrusion and resists water splashes from all directions, <br/>delivering outstanding performance to assist you in dealing with various <br/>unexpected everyday situations.",
+    UseBenefit1: "Wet hand touch",
+    UseBenefit2: "Oily hand touch",
+    UseB2Title: "Drop Resistance <br/>Up to 1.8m",
+    UseB2Desc: "Tough Enough for Every Falls",
+    UseB2Text:
+      "Inside-out high-strength design with exceptional drop <br/>resistance for a reliable, worry-free experience.",
+    CameraTitleBlue: "AI ",
+    CameraTitle: "Imaging",
+    CameraDescPurple: "50MP ",
+    CameraDesc: "AI Camera",
+    CameraText:
+      "50MP Camera, backlight and night scenes are <br/>clearer, and details are restored more accurately.",
+    // picture
+    useImage1: "./assets/section5-use-1.jpg",
+    useImage2: "./assets/section5-use-2.jpg",
+    cameraImage: "./assets/section5-camera.jpg",
+  },
+  // flashSnap
+  section6: {
+    flashTitle: "FlashSnap",
+    flashText:
+      "High-speed shutter, combined with the BestMoment AI algorithm, can help you <br/>capture the wonderful highlights of daily life. Use advanced AI algorithms to <br/>intelligently select the best photos, bringing you a simple and efficient <br/>photography experience.",
+    flashNotice:
+      "*Disclaimer: When the ambient brightness meets conditions, the Snap function button will automatically <br/>appear on the camera interface.",
+    liveTitle: "LivePhoto",
+    liveDesc: "Bring Moments to Life",
+    liveText:
+      "LivePhoto transforms ordinary shots into extraordinary memories. <br/>By recording the moments before and after your shot, LivePhoto <br/>lets you see and hear more of each moment. Image, motion and <br/>sound — LivePhoto helps you to preserve them all.",
+    flashImage: "./assets/section6-flash.jpg",
+    liveImage: "./assets/section6-live.jpg",
+    liveVideo: "./assets/Live_Photo.mp4",
+  },
+  // ai
+  section7: {
+    desc: "AI ",
+    descBlue: "Productivity",
+    card1Title: "Ask Ella",
+    card1Desc: "Solve Problems with one sentence",
+    card1Text:
+      "You can wake Ella up anytime and anywhere using multiple wake-up <br/>methods. Simply give a command in one sentence, and Ella will help <br/>you make it happen.",
+    card1L1: "Photo-based Problem Solving",
+    card1L2: "Photo Editing",
+    card1L3: "Schedule Creating",
+    card1L4: "Navigate",
+    card2Title: "AI Writing",
+    card2Desc: "Short of an Idea? AI Writing Gets It!",
+    card2Text:
+      "It offers Text Generator, Proofread, and Summarize. The tool can be accessed <br/>when text is selected. Its writing styles can adapt to different scenes.",
+    card2L1: "Text Generator",
+    card2L2: "Proofread",
+    card2L3: "Summarize",
+    card3Title: "Hi Translate",
+    card3Desc: "Worry-free Cross-Language Communication",
+    card3Text:
+      "It includes face-to-face translation for offline multi-round exchanges, simultaneous <br/>translation for online meetings, and screenshot/photo translation for mobile selection.",
+    card3L1Num: "136",
+    card3L1Text: "languages",
+    card3L1Desc: "Text Translation",
+    card3L2Num: "167",
+    card3L2Text: "languages",
+    card3L2Desc: "Voice Interpretatation",
+    card3L3Num: "51",
+    card3L3Text: "languages",
+    card3L3Desc: "Photo Translation",
+    card4Title: "AI Call Noise Cancellation",
+    card4Desc: "Reduce Call Noise, Anywhere and Anytime",
+    card4Text:
+      "When in the streets, at malls or at parties, it precisely filters out all background <br/>noises like engines on the road and crowd chatter, and only your voices remain. <br/>Enjoy seamless chats in any noisy setting, free from noise disruptions.",
+    card5Title: "Dynamic Port",
+    card5Desc: "Know What You Want",
+    card5Text:
+      "It acts as a personal assistant that accurately anticipates your needs <br/>and recognizes your intent before you even ask.",
+    card5L1: "Add Contact",
+    card5L2: "Navigate",
+    card5L3: "Translate",
+    card6Title: "Up to 256GB+24GB <br/>(16GB Extended RAM)",
+    card6Text: "Use it smoothly and store as much as you like.",
+    card7Title: "Infrared Remote Control<br/><br/>",
+    card7Text: "The mobile phone is the remote control center.",
+    phoneImage: "./assets/phone-frame.png",
+    microImage: "./assets/phone-camera.png",
+    otherBg1: "./assets/section7-card-6.jpg",
+    otherBg2: "./assets/section7-card-7.jpg",
+    video1: "./assets/video-1.mp4",
+    video2: "./assets/video-2.mp4",
+    video3: "./assets/video-3.mp4",
+    video5: "./assets/video-5.mp4",
+    poster1: "./assets/video-poster-1.jpg",
+    poster2: "./assets/video-poster-2.jpg",
+    poster3: "./assets/video-poster-3.jpg",
+    poster4: "./assets/video-poster-4.jpg",
+    poster5: "./assets/video-poster-5.jpg",
+  },
+  osIcon: "./assets/os-icon.png",
+  sgsIcon: "./assets/sgs-icon.png",
+};
